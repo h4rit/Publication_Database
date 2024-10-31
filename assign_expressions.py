d75fd2dbe336f1829a7a8352d436aa56a2a2b255
Harit Koladia <h4rit.koladia@gmail.com>
2024-10-31 12:00:46 -0700

    Autograder with all testcases

--- assign_expressions.py
+++ assign_expressions.py
@@ -15,6 +15,9 @@ class Expressions:
             ["conference_ranking.conf_abbr", "conference_ranking.rank"]
         )
     
+    
+    #Q1
+    # πname,conference,year,count((σfield='Databases'(field_conference) ⨝ σ2010≤year∧year≤2024(pub_info)) ⨝ σaffiliation='UniversityofCalifornia,Berkeley'(author))
     expression1 =Projection(
                     NaturalJoin(
                         Projection(
@@ -47,14 +50,16 @@ class Expressions:
                     ["pub_info.name", "pub_info.conference","pub_info.year" ,"pub_info.count" ]
                 )
                     
-
-    expression2 = Projection( 
+    # Q2
+    # πconference(σfield_conference.major='Computer Science'(field_conference)) - πconference(σauthor.affiliation='University of California, Irvine'(author⨝pub_info))
+    exp2_1 = Projection( 
                         Selection(
                                 Relation("field_conference"),
                                 Equals("major","Computer Science")
                             ), 
                         ["field_conference.conference"]
-                    ) - Projection(
+                    )
+    exp2_2 = Projection(
                             NaturalJoin(
                                 Relation("pub_info"),
                                 Selection(
@@ -64,7 +69,10 @@ class Expressions:
                             ),
                             ["pub_info.conference"]
                         )
-    
+    expression2 = x - y
+
+    # Q3
+    # πauthor.name,author.affiliation,pub_info.conference,pub_info.year,count(author⨝(pub_info⨝(γfield_conference.conference,pub_info.year;max(pub_info.count)→count(πconference(σfield='Databases'(field_conference))⨝pub_info))))
     # expression3 = ...
     
     # expression4 = ...
@@ -74,6 +82,28 @@ class Expressions:
     # Example query execution
 
 
+# Q4
+# X = author ⨝ ( 
+# σfield='Databases'(field_conference) ⨝ σyear≥2020 ∧ year≤2024(pub_info) )
+
+
+# πaffiliation( X ) 
+ 
+#  -
+ 
+# πaffiliation(
+# πaffiliation( X ) ⨯ πconference(σfield='Databases'(field_conference) ) - πaffiliation,conference( X ) )
+
+# Q5
+# X = πfield_conference.conference(σ(conference_ranking.conference_abbreviation=field_conference.conference)(σfield='Databases'(field_conference)⨝σrank='A*'(conference_ranking)))
+
+# Y= πUniRank1.university_name,UniRank1.rank(ρUniRank1(usnews_university_rankings) ⨝ UniRank1.rank < usnews_university_rankings.rank σuniversity_name = 'UniversityofCalifornia,Irvine' (usnews_university_rankings))
+
+# Z =πpub_info.name,author.affiliation(πpub_info.name(pub_info ⨝ X)⨝author)
+
+# πauthor.affiliation(Y ⨝(UniRank1.university_name=author.affiliation) Z)
+
+
 sql_con = sqlite3.connect('sample220P.db')  # Ensure the uploaded database is loaded here
-result = Expressions.expression1.evaluate(sql_con=sql_con)
+result = Expressions.expression2.evaluate(sql_con=sql_con)
 print(result.rows)
